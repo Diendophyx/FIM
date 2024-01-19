18053b1eaef6bc13e08cb81e55744aec0be2d4c7
diendophyx <henriracine18@gmail.com>
2024-01-19 22:41:44 +1100

    start of the build, have made a function that converts files to hashes using SHA512 and writes them to a text folder, this also creates a baseline everytime you want to add a new one, will need to tweak the /paths in the future to make sure you can use them in any device

---
+++ FIM.ps1
@@ -0,0 +1,41 @@
+﻿
+
+Function Calculate-File-Hash($filepath) {
+    $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
+    return $filehash
+}
+Function Erase-Baseline-If-Already-Exists() {
+    $baselineExists = Test-Path -Path C:\Users\henri\Documents\repos\FIM\baseline.txt
+
+    if ($baselineExists) {
+        # Delete it
+        Remove-Item -Path C:\Users\henri\Documents\repos\FIM\baseline.txt
+    }
+}
+
+
+Write-Host ""
+Write-Host "What would you like to do?"
+Write-Host ""
+Write-Host "    A) Collect new Baseline?"
+Write-Host "    B) Begin monitoring files with saved Baseline?"
+Write-Host ""
+$response = Read-Host -Prompt "Please enter 'A' or 'B'"
+Write-Host ""
+
+if ($response -eq "A".ToUpper()) {
+    # Delete baseline.txt if it already exists
+    Erase-Baseline-If-Already-Exists
+
+    # Calculate Hash from the target files and store in baseline.txt
+    # Collect all files in the target folder
+    $files = Get-ChildItem -Path C:\Users\henri\Documents\FimTest
+
+    # For each file, calculate the hash, and write to baseline.txt
+    foreach ($f in $files) {
+        $hash = Calculate-File-Hash $f.FullName
+        "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath C:\Users\henri\Documents\repos\FIM\baseline.txt -Append
+    }
+    
+}
+
